435013c71d751c20acca0de6da12179b3fa4aae0
JeDTr <tvthong1995@gmail.com>
2019-03-20 22:06:32 +0700

    remove set NODE_ENV in package.json file

--- server.js
+++ server.js
@@ -50,5 +50,4 @@ const port = process.env.PORT || 5000;
 
 app.listen(port, () => {
     console.log(`Server is running n port ${port}`);
-    console.log(path.resolve(__dirname, 'client', 'build', 'index.html'));
 })
