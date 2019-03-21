6b03fcbb6d3d2b66331889f2bf699899d84d942d
JeDTr <tvthong1995@gmail.com>
2019-03-21 14:41:53 +0700

    add build folder

---
+++ client/build/precache-manifest.5e66f39cef30b2eb0a184fb3944b31b2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "9489370649d62bde1068",
+    "url": "/static/js/main.94893706.chunk.js"
+  },
+  {
+    "revision": "7b0120d1a9b31bde8fba",
+    "url": "/static/js/2.7b0120d1.chunk.js"
+  },
+  {
+    "revision": "9489370649d62bde1068",
+    "url": "/static/css/main.6f32de7b.chunk.css"
+  },
+  {
+    "revision": "fa3d1f3b2dc2b19eb53a7993032815a2",
+    "url": "/index.html"
+  }
+];
