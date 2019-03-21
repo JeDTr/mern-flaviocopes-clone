17c8ed617a490528bd96119b9c11067dc70518d4
JeDTr <tvthong1995@gmail.com>
2019-03-21 14:26:11 +0700

    edit / path

--- server.js
+++ server.js
@@ -34,7 +34,7 @@ app.use('/api/tag', tagRoute);
 // if (process.env.NODE_ENV === 'production') {
     app.use(express.static(__dirname + '/client/build'));
     app.get('/', (req, res) => {
-        res.send('Hello World' + path.resolve(__dirname, 'client', 'build', 'index.html'));
+        res.sendFile('Hello World' + path.resolve(__dirname, 'client', 'build', 'index.html'));
     })
     // app.get('*', (req, res) => {
     //   res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
