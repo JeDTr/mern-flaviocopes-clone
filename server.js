f864d272a639886d811962b3e5839f954d567388
JeDTr <tvthong1995@gmail.com>
2019-03-21 14:19:56 +0700

    edit server.js file - remove if process.env.NODE_ENV === production

--- server.js
+++ server.js
@@ -31,12 +31,12 @@ app.use('/api/post', postRoute);
 app.use('/api/tag', tagRoute);
 
 // Server static assets if in production
-if (process.env.NODE_ENV === 'production') {
+// if (process.env.NODE_ENV === 'production') {
     app.use(express.static(__dirname + '/client/build'));
-    app.get('/', (req, res) => {
+    app.get('*', (req, res) => {
       res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
     })
-}
+// }
 
 // DB Config
 const db = require('./config/keys').mongoURI;
