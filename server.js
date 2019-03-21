29032a329304f1dfc8d95bfe3ce7723fe2a95ef6
JeDTr <tvthong1995@gmail.com>
2019-03-21 14:36:51 +0700

    edit / path

--- server.js
+++ server.js
@@ -31,15 +31,15 @@ app.use('/api/post', postRoute);
 app.use('/api/tag', tagRoute);
 
 // Server static assets if in production
-// if (process.env.NODE_ENV === 'production') {
+if (process.env.NODE_ENV === 'production') {
     app.use(express.static(__dirname + '/client/build'));
     app.get('/', (req, res) => {
-        res.sendFile(path.join(__dirname, '/client/build/index.html'));
+        res.send('hello world!' + path.resolve(__dirname, 'client', 'build', 'index.html'));
     })
     // app.get('*', (req, res) => {
     //   res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
     // })
-// }
+}
 
 // DB Config
 const db = require('./config/keys').mongoURI;
