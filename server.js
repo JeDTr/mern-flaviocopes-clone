1c9702068e0acf41967eaa5ef56f0fd3d6483c46
JeDTr <tvthong1995@gmail.com>
2019-03-21 14:22:50 +0700

    edit server add / path

--- server.js
+++ server.js
@@ -33,9 +33,12 @@ app.use('/api/tag', tagRoute);
 // Server static assets if in production
 // if (process.env.NODE_ENV === 'production') {
     app.use(express.static(__dirname + '/client/build'));
-    app.get('*', (req, res) => {
-      res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    app.get('/', (req, res) => {
+        res.send('Hello World');
     })
+    // app.get('*', (req, res) => {
+    //   res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    // })
 // }
 
 // DB Config
